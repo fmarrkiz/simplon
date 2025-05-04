bf61d69eb6d75b95b7e84833c7131a7b8fff50ac
Marie KF <mariekfruchart@outlook.com>
2025-05-04 14:49:50 +0100

    Reorganisation of simplon-related repo

---
+++ front-dev/vscode/exercices.js
@@ -0,0 +1,26 @@
+
+
+/*ne pas oublier les points virgules*/
+let username //prefère username à name
+    admin;
+username = 'John'; 
+let admin; //déclarer avant de copier les valeurs
+admin = username; // ne pas mettre let devant
+alert(admin); //John
+
+
+let planetEarth
+let currentUser
+
+
+let name = "Ilya";
+
+alert(`hello ${1}`); // hello 1
+alert( `hello ${"name"}`); //hello name
+alert(`hello ${name}`) //  hello Ilya
+
+let x =null;
+alert( Number(x) ); // null
+let y;
+alert( Number(y)); // NaN
+
