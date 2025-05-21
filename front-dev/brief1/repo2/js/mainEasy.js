d65d811955231211106c71ba15e5952909498114
Marie KF <mariekfruchart@outlook.com>
2025-05-21 18:15:33 +0100

    (mainEasy.js)added add and delete button to each item, not yet functional though

--- front-dev/brief1/repo2/js/mainEasy.js
+++ front-dev/brief1/repo2/js/mainEasy.js
@@ -4,32 +4,64 @@ let toDoInput = document.querySelector(".toDoInput") ;
 let toDoButton = document.querySelector(".toDoButton");
 let ToDoList = document.querySelector(".ToDoList");
 
-toDoButton.addEventListener("click", addToDo);
+
+toDoButton.addEventListener("click", addToDoTask);
 toDoInput.addEventListener("keypress", function (e) {
     if (e.key === "Enter") {
-        addToDo(e);
+        addToDoTask(e);
     }
 }
 ); 
   
 
-function addToDo (event) {
+function addToDoTask (event) {
 event.preventDefault();
 
 let inputValue = toDoInput.value.trim();
 if (inputValue === "") {
-    alert("Oups! Tu n'as pas écrit de texte");
+    alert ("Pense à bien compléter ta liste avant de partir!");
 } else {
 
-let NewItem = document.createElement("li");
-NewItem.innerText = inputValue;
-ToDoList.appendChild(NewItem);
+    let NewItem = document.createElement("li");
+    NewItem.innerText = inputValue;
+    ToDoList.appendChild(NewItem);
 
-toDoInput.value = "";
+    let addButton = document.createElement("button");
+    addButton.innerHTML = "&#8722";
+    ToDoList.appendChild(addButton);
+    addButton.addEventListener("click", addQuantity);
 
-}
+    let deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "&#43";
+    ToDoList.appendChild(deleteButton);
+    deleteButton.addEventListener("click", function () {
+       ToDoList.removeChild(NewItem);
+       ToDoList.removeChild(deleteButton);
 
+    toDoInput.value = "";
+         });
 
+       }
 } 
 
 
+function addQuantity() {
+
+}
+    
+function deleteQuantity() {
+    if (TaskQuantity === 1) {
+        deleteButton.addEventListener("click", function () {
+            ToDoList.removeChild(NewItem);
+            ToDoList.removeChild(deleteButton);
+        
+    });
+
+} else if (TaskQuantity > 1) {
+        deleteButton.addEventListener("click", function () {
+           
+        
+    });
+
+}
+}
